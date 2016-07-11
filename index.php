f79c2e6afb20581bc93445cbde0453818fcc76b8
bz4work@gmail.com <bz4work@gmail.com>
2016-07-12 00:59:30 +0300

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slim
+ * Date: 11.07.2016
+ * Time: 23:49
+ */
+
+
+
+
+
+
+?>
+
+<form method="POST" action="test.php">
+    <input type="text" name="url" value="http://aet.ua/robots.txt">
+    <input type="submit" value="go">
+</form>
+
+<?php
+
+
+
+
+
+?>
